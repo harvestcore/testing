e07f779a943fa8b14720b0591176cddf502ee7e9
harvestcore <aagomezies@gmail.com>
2020-05-31 12:04:58 +0200

    cambio deploy test

--- cypress/integration/deploy.spec.js
+++ cypress/integration/deploy.spec.js
@@ -1,135 +1,55 @@
 /// <reference types="cypress" />
 
-context('Machines panel', () => {
+context('Status', () => {
   before(() => {
     cy.login();
   });
 
-  it('Go to machines panel', () => {
+  it('Go home', () => {
     cy.getCookie('ipm-token').should('be.not.equal', null);
-    cy.visit('http://localhost:4200/deploy');
+    cy.visit('http://localhost:4200');
   });
 
-  it('check if main card exists', () => {
-    cy.get('mat-card').contains('Deploy').should('exist');
+  it('check that docker card exists', () => {
+    cy.get('span').contains('Docker service').should('exist');
   });
 
-  it('check that main buttons exists', () => {
-    cy.get('[mattooltip="Run containers"] > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa > path').should('exist');
-    cy.get('[mattooltip="Refresh data"] > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa').should('exist');
-    cy.get('.mat-warn > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa > path').should('exist');
+  it('check that mongo card exists', () => {
+    cy.get('span').contains('Mongo service').should('exist');
   });
 
-  it('check that filter exists', () => {
-    cy.get('mat-label').contains('Filter').should('exist');
-    cy.get('[icon="times"]').should('exist');
+  it('check that mongo card has its fields', () => {
+    cy.get('mat-list-item').contains('Customers').should('exist');
+    cy.get('mat-list-item').contains('Users in current customer').should('exist');
+    cy.get('mat-list-item').contains('Databases').should('exist');
   });
 
-  it('check that columns selector exists', () => {
-    cy.get('mat-label').contains('Columns').should('exist');
-    cy.get('mat-select').should('exist');
+  it('check that the user button exists', () => {
+    cy.get('button').contains('admin (').should('exist');
   });
 
-  it('ensure that there are no containers', () => {
-    // Remove any possible second user
-    cy.get('.mat-warn').click();
-    cy.get('.mat-dialog-actions > .mat-warn').click();
-    cy.wait(500);
+  it('check that the logout button exists', () => {
+    cy.get('button').contains('admin (').click();
+    cy.get('button').contains('Logout').should('exist');
   });
 
-  it('check images buttons and go back containers', () => {
-    // Remove any possible second user
-    cy.get('#mat-tab-label-0-1').click();
-    cy.wait(500);
-
-    cy.get('[mattooltip="Search and pull images"] > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa > path').should('exist');
-    cy.get('[mattooltip="Refresh data"] > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa').should('exist');
-    cy.get('.imageButtons > .mat-warn > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa > path').should('exist');
-
-    cy.get('#mat-tab-label-0-0').click();
+  it('check that the admin user button exists', () => {
+    cy.get('button').contains('Admin').should('exist');
   });
 
-  it('download image and run it, after that go back to containers', () => {
-    // Remove any possible second user
-    cy.get('#mat-tab-label-0-1').click();
-    cy.wait(500);
-
-    cy.get('[mattooltip="Search and pull images"] > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa').click();
-    cy.get('#searchTerm').type('harvestcore{enter}');
-    cy.get('.table > app-ipmtable.ng-star-inserted > .mat-table > tbody > :nth-child(1) > .cdk-column-actions > div.ng-star-inserted > .mat-focus-indicator > .mat-button-wrapper > .ng-fa-icon > .svg-inline--fa').click();
-    cy.wait(5000);
-    cy.get('.mat-dialog-actions > .mat-focus-indicator > .mat-button-wrapper').click();
-
-    cy.get('#mat-tab-label-0-0').click();
-    cy.wait(500);
-    cy.get('#mat-tab-label-0-1').click();
-    cy.wait(500);
-    cy.get('.cdk-column-tags > .mat-sort-header-container > .mat-sort-header-button').click();
-    cy.wait(100);
-    cy.get(':nth-child(1) > .cdk-column-actions > div.ng-star-inserted > .mat-accent').click();
-
-    cy.get('#mat-tab-label-0-0').click();
-    cy.wait(500);
+  it('check that the home button exists', () => {
+    cy.get('button').contains('IPManager').should('exist');
   });
 
-  it('manage container', () => {
-    cy.get('td').contains('running').should('exist');
-    cy.get('.cdk-column-image_tag > .mat-sort-header-container > .mat-sort-header-button').click();
-    cy.get(':nth-child(1) > .cdk-column-actions > div.ng-star-inserted > .mat-focus-indicator').click();
-
-    cy.get('button').contains('Pause').click();
-    cy.wait(1000);
-    cy.get('button').contains('Unpause').click();
-    cy.wait(1000);
-    cy.get('button').contains('Kill').click();
-    cy.wait(1000);
-    cy.get('button').contains('Restart').click();
-    cy.wait(1000);
-    cy.get('button').contains('Stop').click();
-    cy.wait(10000);
-    cy.get('button').contains('Restart').click();
-    cy.get('button').contains('Restart').click();
-    cy.wait(1000);
-    cy.get('button').contains('Close').click();
-    cy.wait(500);
+  it('check that the deploy button exists', () => {
+    cy.get('button').contains('Deploy').should('exist');
   });
 
-  it('rename container', () => {
-    cy.get('#mat-tab-label-0-1').click();
-    cy.wait(500);
-    cy.get('#mat-tab-label-0-0').click();
-    cy.wait(500);
-    cy.get('td').contains('running').should('exist');
-    cy.get('.cdk-column-image_tag > .mat-sort-header-container > .mat-sort-header-button').click();
-    cy.wait(500);
-    cy.get(':nth-child(1) > .cdk-column-actions > div.ng-star-inserted > .mat-focus-indicator').click();
-    cy.wait(500);
-    cy.get('#mat-expansion-panel-header-4 > .mat-content > .mat-expansion-panel-header-title').click();
-    cy.get('#name').clear();
-    cy.get('#name').type('testing_container');
-    cy.get('[icon="check"]').click();
-    cy.wait(250);
-    cy.get('#mat-dialog-title-3').contains('testing_container').should('exist');
-    cy.get('button').contains('Close').click();
-    cy.wait(250);
+  it('check that the provision button exists', () => {
+    cy.get('button').contains('Provision').should('exist');
   });
 
-  it('kill container and prune', () => {
-    cy.get('#mat-tab-label-0-1').click();
-    cy.wait(500);
-    cy.get('#mat-tab-label-0-0').click();
-    cy.wait(500);
-    cy.get('td').contains('running').should('exist');
-    cy.get('.cdk-column-image_tag > .mat-sort-header-container > .mat-sort-header-button').click();
-    cy.wait(500);
-    cy.get(':nth-child(1) > .cdk-column-actions > div.ng-star-inserted > .mat-focus-indicator').click();
-    cy.wait(500);
-    cy.get('button').contains('Kill').click();
-    cy.wait(1000);
-    cy.get('button').contains('Close').click();
-    cy.wait(250);
-    cy.get('.mat-warn').click();
-    cy.get('.mat-dialog-actions > .mat-warn').click();
-    cy.wait(500);
+  it('check that the machines button exists', () => {
+    cy.get('button').contains('Machines').should('exist');
   });
-});
+})
